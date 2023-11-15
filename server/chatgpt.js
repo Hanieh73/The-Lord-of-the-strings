87d288b7262b700713299cd9b1cf83c78ee5810d
Nadim-001 <mdnadimahmed.001@gmail.com>
2023-11-15 16:07:41 +0000

    Refactored chatgpt.js

--- server/chatgpt.js
+++ server/chatgpt.js
@@ -10,18 +10,7 @@ const chat = async (conversation) => {
   const res = await openai.chat.completions.create({
     //pop our code to get the chat
     model: 'gpt-3.5-turbo',
-    messages: [
-      // {
-      //   role: 'system',
-      //   content:
-      //     'You are John Wick from the John Wick movies. You are to answer questions as this character. Do not break character. Please return your answer formatted as html including p tags for new paragraphs',
-      // },
-      // {
-      //   role: 'user',
-      //   content: question,
-      // },
-      ...conversation,
-    ],
+    messages: [...conversation],
   });
 
   return res.choices[0].message.content;
