0b198754c9131f713a407acc4b2e816f6fa60497
Tayeeb Islam <54636425+Tayeeb27@users.noreply.github.com>
2023-11-22 09:09:05 +0000

    Fixed dialogue

--- client/src/pages/GamePage/index.jsx
+++ client/src/pages/GamePage/index.jsx
@@ -29,8 +29,6 @@ import {BackgroundMusic} from'../../assets'
 
 const GamePage = () => {
   const [audioPlayed, setAudioPlayed] = useState(false);
-
-  
   const [dialogue, setDialogue] = useState('Enter Continue');
   const [userInput, setUserInput] = useState('');
   const [visibleUserInput, setVisibleUserInput] = useState('Enter Continue');
@@ -39,26 +37,10 @@ const GamePage = () => {
   const [conversation, setConversation] = useState([
     {
       role: 'system',
-      content: `"In 'City 72,' a text-based adventure game, you are the story master, guiding the narrative and presenting choices to players. Each choice, labeled A, B, and C, leads to new story developments. When a player sends 'h' or 'hint,' instead of the labeled choice the game will integrate a hint into the next narrative, providing guidance relevant to their current situation. This hint is offered only once per puzzle or scenario. and the game continues with the standard narrative and choice options. Format the response as a string JSON object with the keys: 'current_location', ''act', 'storyname', 'narrative', 'items', 'character', 'choices', and 'achievements' where applicable. ${mainStory}, ${heistStory}, ${warStory}, ${techMagiStory}, ${achievements}. Response format:{
-        "current_location": "The current character location, e.g., 'Neon Streets of City 72'",
-        "act": "current act e.g. Act 1",
-        "storyname": "current story"
-        "narrative": "Narrative text describing the scene, including a hint if requested, e.g., 'Lost in the labyrinthine paths, Renn notices a pattern in the wall markings, hinting at a hidden exit.'",
-        "items": ["List of items currently in the player's possession"],
-        "character": "Name of the main character or character being interacted with",
-        "choices": [
-          {"A": "Description of Choice A and its potential outcomes"},
-          {"B": "Description of Choice B and its potential outcomes"},
-          {"C": "Description of Choice C and its potential outcomes"}
-        ],
-        "achievements": {
-          "name": "Name of the achievement unlocked",
-          "achieved": true
-        }
-      }
-
-      Jump to a random scene in the main story"
-`,
+      content: `I will provide a story setting, and your role is to act as the story master, guiding the narrative and presenting choices to the players. Each choice, labeled A, B, and C, leads to new developments in the story. 
+      Format the response JSON object with the keys: 'current_location', 'narrative', 'items', 'character', 'choices'.The name of these keys must be stored exactly like this. Here's the story prompt: ${mainStory} \n\nResponse format:\n\n{\n  'current_location': 'the current character location with only the location names in ${locationImages}',
+      \n'narrative': 'Approaching City 72, athe adventurer turned to the crowded streets. Start every narrative except the first one with You chose: the message.content that the user gives and then what the user selected,\n\nthen the narrative  ',\n'items': ['List of currently possessed items'],\n'character': 'Main character name or name of character being interacted with by main character',\n'choices': [\n{'A': 'Choice A description'},
+      \n{'B': 'Choice B description'},\n{'C': 'Choice C description'}\n]\n }`,
     },
   ]);
 
@@ -68,7 +50,6 @@ const GamePage = () => {
   const [characterdisplayed, setCharacterdisplayed] = useState(rennharlow);
   const [characterName, setCharacterName] = useState('Renn Harlow');
   const [choices, setChoices] = useState([]);
-  console.log(characterdisplayed);
   // const { currentGameID } = useExample();
   // const [saveData, setSaveData] = useState();
 
@@ -122,14 +103,14 @@ const GamePage = () => {
   }, []);
 
   let choiceMade = ""
+  const [choicesMade, setChoicesMade] = useState([]);
   const submitUserInput = async () => {
     if (userInput.trim() !== '') {
       // Extract the choice from the user input
       const userChoice = userInput.trim().toUpperCase();
-      
       // Show the choice in visibleUserInput
       if (userChoice == "CONTINUE") {
-        setVisibleUserInput(`You Chose: ${userChoice}`);
+        setVisibleUserInput(`You Chose: ${userChoice}`);   
       } else if (userChoice == "A" || userChoice == "B" || userChoice == "C") {
         const choiceIndex = userChoice.charCodeAt(0) - 'A'.charCodeAt(0);
         choiceMade =choices[choiceIndex][userChoice]
@@ -137,7 +118,7 @@ const GamePage = () => {
       } else {
         setVisibleUserInput(`You Chose: ${choiceMade}`);
       }
-      const userMessage = { role: 'user', content: userInput};
+      const userMessage = { role: 'user', content: userInput };
       setConversation((prevConversation) => [...prevConversation, userMessage]);
       try {
         const response = await fetch('https://city-72-wez6.onrender.com/chats', {
@@ -155,8 +136,6 @@ const GamePage = () => {
           ? formatedData.current_location.replace(/\s/g, '').toLowerCase()
           : '';
         setLocation(locationImages[formattedLocation]);
-        console.log('Formatted Location:', formattedLocation.toString());
-        console.log('Location Image:', locationImages[formattedLocation.toString()]);
   
         setDialogue(formatedData.narrative);
   
@@ -166,7 +145,7 @@ const GamePage = () => {
         const itemsArray = formattedItems.map((item) => itemImages[item]);
         setItems(itemsArray);
   
-        if (formatedData.character == ""||formatedData.character == null) {
+        if (formatedData.character == '') {
           setCharacterName('Renn Harlow');
           setCharacterdisplayed('rennharlow');
         } else {
@@ -177,6 +156,7 @@ const GamePage = () => {
           setCharacterName(formatedData.character);
         }
         setChoices(formatedData.choices);
+        
         setConversation((prevConversation) => [
           ...prevConversation,
           { role: 'assistant', content: data.message },
@@ -186,10 +166,12 @@ const GamePage = () => {
         console.error('Error fetching response:', error);
       }
       console.log(conversation);
+      console.log(choicesMade);
+
     }
   };
 
-  const userChoiceIsABC = (choice) => ['A', 'B', 'C'].includes(choice);
+
   const toggleInventory = () => {
     setInventoryVisible(!inventoryVisible);
   };
@@ -226,13 +208,6 @@ const GamePage = () => {
           <div className='conversation'>
             {conversation.map((message, index) => (
               <div key={index}>
-                {message.role === 'user' && (
-              <p>
-                User: {userChoiceIsABC(message.content)
-                  ? choices[message.content.charCodeAt(0) - 'A'.charCodeAt(0)][message.content]
-                  : message.content}
-              </p>
-            )}
                 {message.role === 'assistant' && 
                   <TypeAnimation
                     key={index}  // Use the index as the key
